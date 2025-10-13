37a368653ef857af2c36c2364c82d5f546c610c0
Tolera Girma <tolergirm@gmail.com>
2025-10-13 23:16:01 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+   int x = 10;
+   int y = x++;
+   int z = ++x;
+    cout << "x: " << x <<endl;
+    cout<<"y: " << y << endl;
+    cout<<"z: " << z << endl;
+
+    return 0;
+}
+// This program prints a greeting message to the console.
